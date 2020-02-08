3912eeb142edce3416055fc217d377f46764ffd1
sasha walker <sashawalker31@gmail.com>
2020-02-08 02:29:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Dog 
+  def name= (dog_name)
+    @this_dog_name = dog_name
+    
+  end 
+  
+  def name 
+    @this_dog_name
+  end 
+end
+
+class Dog 
+  def bark 
+    puts "woof!"
+  end
+end
